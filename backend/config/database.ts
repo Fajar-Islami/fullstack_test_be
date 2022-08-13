dbfe752b3923847b0dd21526ab635c2e2729a29f
Fajar-Islami <ahmadfajarislami@protonmail.com>
2022-08-13 21:14:52 +0700

    selesai database

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,11 +1,30 @@
-import path from 'path';
-
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      timezone: env("DATABASE_TIMEZONE"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not Required
+      ssl: env("DATABASE_SSL", false),
     },
+    Optional: {
+      // debug: true,
+      pool: {
+        min: 2,
+        max: 30,
+        createTimeoutMillis: 3000,
+        acquireTimeoutMillis: 30000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100,
+        propagateCreateError: false,
+      },
+    },
+    debug: false,
     useNullAsDefault: true,
   },
 });
---
+++ backend/dist/config/database.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    connection: {
+        client: "postgres",
+        connection: {
+            host: env("DATABASE_HOST"),
+            port: env.int("DATABASE_PORT"),
+            database: env("DATABASE_NAME"),
+            user: env("DATABASE_USERNAME"),
+            password: env("DATABASE_PASSWORD"),
+            timezone: env("DATABASE_TIMEZONE"),
+            schema: env("DATABASE_SCHEMA", "public"),
+            ssl: env("DATABASE_SSL", false),
+        },
+        Optional: {
+            // debug: true,
+            pool: {
+                min: 2,
+                max: 30,
+                createTimeoutMillis: 3000,
+                acquireTimeoutMillis: 30000,
+                idleTimeoutMillis: 30000,
+                reapIntervalMillis: 1000,
+                createRetryIntervalMillis: 100,
+                propagateCreateError: false,
+            },
+        },
+        debug: false,
+        useNullAsDefault: true,
+    },
+});
